cffd6df23290918481df16c4760619144fff5f40
Yaniv BRONSHTEIN <ybrons1@ds2.CS.McGill.CA>
2017-12-04 17:52:08 -0500

    Created fork.h

---
+++ fork.h
@@ -0,0 +1,15 @@
+/*
+ * Name: Joshua Friedman and Yaniv Bronshtein
+ * ID's: 260738744 and 260618099
+ * Date: Dec 4, 2017
+ * Version: 1.0
+ * Description: fork.h contains the declarations of the functions in Producer.c and Consumer.c. It also
+ * defines two constants for the Producer and Consumer turns*/
+
+#ifndef __ASS6_FORK_H__
+#define __ASS6_FORK_H__
+#define PRODUCER '0'
+#define CONSUMER '1'
+int producer();
+int consumer();
+#endif //__ASS6_FORK_H__
